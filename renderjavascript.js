ca220f5fbe40061d25002b5c208ecfcd6ecf6188
ruangsehatku <ziebuss@gmail.com>
2017-04-04 00:19:28 +0700

    Add files via upload

---
+++ renderjavascript.js
@@ -0,0 +1,5 @@
+//<![CDATA[
+//CSS Ready
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+loadCSS("https://fonts.googleapis.com/css?family=Raleway");loadCSS("https://fonts.googleapis.com/css?family=Open+Sans:300,300i,600,600i,800%7CMaterial+Icons");
+//]]>
